5b47c3d6b86ffc0d77059cbf86dd143932141384
Saikat <saikatsaha.rph@gmail.com>
2026-06-08 10:39:12 +0530

    solved protected route redirecting logged user to login page on each refresh problem

--- src/components/utils/ProtectedRoutes.jsx
+++ src/components/utils/ProtectedRoutes.jsx
@@ -2,14 +2,21 @@ import { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { AuthContext } from '../../context/AuthContext';
-
+import { Loader2 } from 'lucide-react';
 
 
 const ProtectedDocRoute = ({ children }) => {
   const location = useLocation();
-  const { isAuthenticated } = useContext(AuthContext);
+  const { isAuthenticated, loader } = useContext(AuthContext);
 
-  return isAuthenticated ? children : <Navigate to="/login" state={{from : location}} replace/>;
+  if (loader) {
+    return <div className='flex flex-col justify-center items-center h-screen gap-4 text-white/50'>
+      <Loader2 className='h-8 w-8 animate-spin' />
+      <p>Checking authentication...</p>
+    </div>
+  }
+
+  return isAuthenticated ? children : <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 
